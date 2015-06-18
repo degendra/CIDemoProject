2c450e6ba12abd50d8b30c40237d774e84e7a8fd
Degendra <degendra@bajratechnologies.com>
2015-06-18 18:13:57 +0545

    Initial commit

---
+++ app/src/main/java/np/pro/degendra/cidemoproject/User.java
@@ -0,0 +1,13 @@
+package np.pro.degendra.cidemoproject;
+
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
---
+++ app/src/test/java/np/pro/degendra/cidemoproject/UserTest.java
@@ -0,0 +1,27 @@
+package np.pro.degendra.cidemoproject;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(emulateSdk = 18, reportSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class UserTest {
+
+    @Test
+    public void UserNameTest() {
+        User u = new User();
+
+        u.setName("foo");
+
+        Assert.assertEquals("foo", u.getName());
+    }
+
+    @Test
+    public void failingTest() {
+        Assert.assertFalse(true);
+    }
+}
