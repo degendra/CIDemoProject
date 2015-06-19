0a543229c4966fa6aadff0bb0f5ed64b3e2370b9
Degendra <degendra@bajratechnologies.com>
2015-06-19 11:25:13 +0545

    minsdk for test changed to 15 instead of 18

--- app/src/test/java/np/pro/degendra/cidemoproject/UserTest.java
+++ app/src/test/java/np/pro/degendra/cidemoproject/UserTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-@Config(emulateSdk = 18, reportSdk = 18)
+@Config(emulateSdk = 15, reportSdk = 15)
 @RunWith(RobolectricTestRunner.class)
 public class UserTest {
 
